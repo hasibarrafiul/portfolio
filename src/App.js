343d8e9026fd5a36d0d48168076acf3a2023dcfe
hasibarrafiul <fahad.com66.fk@gmail.com>
2024-05-11 02:59:34 +0900

    second page added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [animate, setAnimate] = useState(false);
   const [spritePositionX, setSpritePositionX] = useState(50);
   const [mirror, setMirror] = useState(false);
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -37,6 +38,29 @@ function App() {
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, []);
+  console.log(pageNumber);
+  console.log(spritePositionX);
+  
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if ((event.key === 'Enter' || event.key === ' ') && spritePositionX > 90) {
+        setPageNumber(pageNumber + 1);
+        setSpritePositionX(10);
+      }
+      if ((event.key === 'Enter' || event.key === ' ') && spritePositionX < 10) {
+        if(pageNumber>1){
+          setPageNumber(pageNumber - 1);
+          setSpritePositionX(90);
+        }
+      }
+    };
+  
+    window.addEventListener('keydown', handleKeyDown);
+  
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [spritePositionX, pageNumber]);
 
   return (
     <div className="custom-background">
@@ -44,8 +68,14 @@ function App() {
         <h1 className="welcomeText">Welcome to my portfolio game</h1>
         <img src={loadingGif} alt="Loading" className="loadingBar" />
       </div>
-
-      <div className={`mainMenu ${animate ? 'animateBottom' : ''}`}>
+      {pageNumber === 1 && (
+      <div className={`mainMenu ${animate ? 'animateBottom' : ''} ${pageNumber === 2 ? 'slideLeft' : ''}`}>
+        <div className='myInformationScreen'>
+            <h1 className="myName">My name is Hasib Ar Rafiul Fahim</h1>
+            <h3 className="myTitle">I am a Software Engineer and Game Programmer</h3>
+            <p className="myDescription">I am passionate about software engineering and game programming. I love creating games and solving problems.</p>
+            <p className="myDescription">I currently live in Tokyo, Japan.</p>
+        </div>
         <div className="platformContainer">
           <img src={platform} alt="Platform" className="platform" />
           <img src={gate} alt="Gate" className="gate" />
@@ -55,6 +85,20 @@ function App() {
           }} />
         </div>
       </div>
+      )}
+      {pageNumber === 2 && (
+          <div className={`careerDiv ${pageNumber === 2 ? 'slideRight' : ''}`}>
+          <div className="platformContainer">
+          <img src={platform} alt="Platform" className="platform" />
+          <img src={gate} alt="Gate" className="gate" />
+          <img src={gate} alt="Gate2" className="gate2" />
+          <img src={sprite} alt="Sprite" className="sprite" style={{
+            left: `${spritePositionX}%`,
+            transform: `translateX(-50%) scaleX(${mirror ? '-1' : '1'})`
+          }} />
+        </div>
+      </div>
+      )}
     </div>
   );
 }
