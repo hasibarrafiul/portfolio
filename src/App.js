ea195834aa742e2a9a520edbd84622ce36518fa3
hasibarrafiul <fahad.com66.fk@gmail.com>
2024-05-12 02:22:56 +0900

    skill page added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import techrz from './images/info/techrz.png';
 import sysystem from './images/info/sysystem.png';
 import seeMyCareer from './images/ui/seeMyCareer.png';
 import backToMainScreen from './images/ui/backToMainScreen.png';
+import goToSkillPage from './images/ui/goToSkillPage.png';
+import goBackToCareer from './images/ui/goBackToCareer.png';
+
+import ProgressBar from "@ramonak/react-progress-bar";
 
 
 function App() {
@@ -18,6 +22,7 @@ function App() {
   const [spritePositionX, setSpritePositionX] = useState(50);
   const [mirror, setMirror] = useState(false);
   const [pageNumber, setPageNumber] = useState(1);
+  const [prevPage, setPrevPage] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -46,16 +51,16 @@ function App() {
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, []);
-  console.log(pageNumber);
-  console.log(spritePositionX);
   
   useEffect(() => {
     if (spritePositionX > 90) {
+        setPrevPage(pageNumber);
         setPageNumber(pageNumber + 1);
         setSpritePositionX(10);
       }
       if (spritePositionX < 10) {
         if(pageNumber>1){
+          setPrevPage(pageNumber);
           setPageNumber(pageNumber - 1);
           setSpritePositionX(90);
         }
@@ -69,7 +74,7 @@ function App() {
         <img src={loadingGif} alt="Loading" className="loadingBar" />
       </div>
       {pageNumber === 1 && (
-      <div className={`mainMenu ${animate ? 'animateBottom' : ''} ${pageNumber === 2 ? 'slideLeft' : ''}`}>
+      <div className={`mainMenu ${animate ? 'animateBottom' :''}`}>
         <div className='myInformationScreen'>
             <h1 className="myName">My name is Hasib Ar Rafiul Fahim</h1>
             <h3 className="myTitle">I am a Software Engineer and Game Programmer</h3>
@@ -79,7 +84,7 @@ function App() {
         <div className="platformContainer">
           <img src={platform} alt="Platform" className="platform" />
           <img src={gate} alt="Gate" className="gate" />
-            <img src={seeMyCareer} alt="seeMyCareer" className="seeMyCareer" />
+            <img src={seeMyCareer} alt="seeMyCareer" className="forwardSign" />
           <img src={sprite} alt="Sprite" className="sprite" style={{
             left: `${spritePositionX}%`,
             transform: `translateX(-50%) scaleX(${mirror ? '-1' : '1'})`
@@ -88,7 +93,7 @@ function App() {
       </div>
       )}
       {pageNumber === 2 && (
-          <div className={`careerDiv ${pageNumber === 2 ? 'slideRight' : ''} custom-background2`}>
+          <div className={`careerDiv ${prevPage > pageNumber ? 'slideLeft' : 'slideRight'} custom-background2`}>
           <div className="platformContainer">
           <img src={platform} alt="Platform" className="platform" />
           <img src={studyImage} alt="study" className="study" />
@@ -97,7 +102,34 @@ function App() {
           <img src={techrz} alt="techrz" className="techrz" />
           <img src={sysystem} alt="sysystem" className="sysystem" />
           
-          <img src={backToMainScreen} alt="backToMainScreen" className="backToMainScreen" />
+          <img src={backToMainScreen} alt="backToMainScreen" className="backWordSign" />
+          <img src={goToSkillPage} alt="goToSkillPage" className="forwardSign" />
+          <img src={gate} alt="Gate" className="gate" />
+          <img src={gate} alt="Gate2" className="gate2" />
+          <img src={sprite} alt="Sprite" className="sprite" style={{
+            left: `${spritePositionX}%`,
+            transform: `translateX(-50%) scaleX(${mirror ? '-1' : '1'})`
+          }} />
+        </div>
+      </div>
+      )}
+
+        {pageNumber === 3 && (
+          <div className={`skillsbar ${prevPage > pageNumber ? 'slideLeft' : 'slideRight'} custom-background2`}>
+          <div className="platformContainer">
+          <img src={platform} alt="Platform" className="platform" />
+          <h1 className="skillsTitle">Skills</h1>
+          <ProgressBar className="java" completed={80} customLabel="Java" />
+          <ProgressBar className="cSharp" completed={60} customLabel="C#" />
+          <ProgressBar className="python" completed={70} customLabel="Python" />
+          <ProgressBar className="javaScript" completed={80} customLabel="JavaScript" />
+
+          <ProgressBar className="unity" completed={80} customLabel="Unity" />
+          <ProgressBar className="unreal" completed={60} customLabel="Unreal Engine" />
+          <ProgressBar className="django" completed={80} customLabel="Django" />
+          <ProgressBar className="react" completed={80} customLabel="React" />
+          
+          <img src={goBackToCareer} alt="goBackToCareer" className="backWordSign" />
           <img src={gate} alt="Gate" className="gate" />
           <img src={gate} alt="Gate2" className="gate2" />
           <img src={sprite} alt="Sprite" className="sprite" style={{
