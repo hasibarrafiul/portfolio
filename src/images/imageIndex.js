d9c8b1d9bc33ddaae3a176d4899f4830a28bf792
hasibarrafiul <fahad.com66.fk@gmail.com>
2026-05-19 00:01:12 +0900

    removed the unused assests

--- src/images/imageIndex.js
+++ src/images/imageIndex.js
@@ -6,15 +6,3 @@ export { default as sprite } from './assets/sprite.png';
 export { default as spriteRun } from './assets/spriteRun.png';
 export { default as gate } from './assets/gate.png';
 export { default as rotateNotice } from './assets/rotateNotice.gif';
-
-// UI Navigation
-export { default as seeMyCareer } from './ui/seeMyCareer.png';
-export { default as backToMainScreen } from './ui/backToMainScreen.png';
-export { default as goToSkillPage } from './ui/goToSkillPage.png';
-export { default as goBackToCareer } from './ui/goBackToCareer.png';
-export { default as gotoProjects } from './ui/gotoProjects.png';
-export { default as goBackToSkills } from './ui/goBackToSkills.png';
-export { default as goToLicenceAndCertifications } from './ui/goToLicenceAndCertifications.png';
-export { default as goBackToProjects } from './ui/goBackToProjects.png';
-export { default as goToContactAndLinks } from './ui/goToContactAndLinks.png';
-export { default as goBackToLicenceAndCertifications } from './ui/goBackToLicenseAndCertifications.png';
